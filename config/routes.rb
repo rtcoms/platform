4cd1c99530e8e2a2af25760ea9a0afdb060a1ef2
Rohit Trivedi <86.rohit@gmail.com>
2014-03-16 12:53:34 +0530

    fetching linkedin data implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Platform::Application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "authorizations" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  devise_for :users, controllers: { omniauth_callbacks: "authorizations" }
+  match '/fetch/linkedin' => 'authorizations#fetch_linkedin', via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
